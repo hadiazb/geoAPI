2e8f8dd1f0e34fca4f273871f4372d00fc8331ad
hugo <hugoandresdiazbernal@gmail.com>
2020-05-17 00:08:05 -0500

    Filtrando todos los paises y provincias del mundo

--- index.js
+++ index.js
@@ -21,60 +21,74 @@ function traer() {
     }
   })
 }
-// traer();
+traer();
 
-  // location
-  // country_code
-  // latitude
-  // longitude
-  // recovered
-  // dead 
-  // confirmed
-
-
-// renderData()
+renderData()
 async function getData() {
   const response = await fetch('https://www.trackcorona.live/api/provinces')
   const data = await response.json()
-  // console.log(data.data.length)
+  var dataBase = [
+    {
+      "confirmed": null,
+      "country_code": null,
+      "dead": null,
+      "latitude": null,
+      "location": null,
+      "longitude": null,
+      "recovered": null,
+      "updated": null,
+
+    },
+  ]
+
   for (let i = 0; i < data.data.length; i++) {
     if (data.data[i].country_code == "us") {
-      // console.log(data.data[i]);
-      console.log(data.data[i]);
-      // return data.data
+        var place = { 
+          "confirmed":     data.data[i].confirmed,
+          "country_code":  data.data[i].country_code,
+          "dead":          data.data[i].dead,
+          "latitude":      data.data[i].latitude,
+          "location":      data.data[i].location,
+          "longitude":     data.data[i].longitude,
+          "recovered":     data.data[i].recovered,
+          "updated":       data.data[i].updated,
+        }
+        dataBase.push(place);
+      }
     }
-  }
+  // console.log(dataBase)
+  return dataBase
 }
-getData()
+// getData()
 
-// function renderExtraData({ location, country_code, recovered, dead, confirmed }) {
-//   return (`
-//     <div>
-//       <p> <strong>${country_code} - ${location}</strong> </p>
-//       <p> confirmados: ${confirmed} </p>
-//       <p> muertes: ${dead} </p>
-//       <p> recuperados: ${recovered} </p>
-//     </div>
-//   `)
-// }
-// const icon = './icon.png'
-// const popup = new window.google.maps.InfoWindow()
+function renderExtraData({ location, country_code, recovered, dead, confirmed }) {
+  return (`
+    <div>
+      <p> <strong>${country_code} - ${location}</strong> </p>
+      <p> confirmados: ${confirmed} </p>
+      <p> muertes: ${dead} </p>
+      <p> recuperados: ${recovered} </p>
+    </div>
+  `)
+}
+const icon = 'https://raw.githubusercontent.com/LeonidasEsteban/covid-19-map/master/icon.png'
+const popup = new window.google.maps.InfoWindow()
 
-// async function renderData() {
-//   const data = await getData() 
-//   data.forEach(item => {
-//     const marker = new window.google.maps.Marker({
-//       position: {
-//         lat: item.latitude,
-//         lng: item.longitude,
-//       },
-//       map,
-//       // icon,
-//       title: String(item.confirmed),
-//     })
-//     marker.addListener('click', () => {
-//       popup.setContent(renderExtraData(item))
-//       popup.open(map, marker)
-//     })
-//   })
-// }
+async function renderData() {
+  const data = await getData() 
+  data.forEach(item => {
+    const marker = new window.google.maps.Marker({
+      position: {
+        lat: item.latitude,
+        lng: item.longitude,
+      },
+      map,
+      icon,
+      title: String(item.confirmed),
+    })
+    marker.addListener('click', () => {
+      popup.setContent(renderExtraData(item))
+      popup.open(map, marker)
+    })
+  })
+}
